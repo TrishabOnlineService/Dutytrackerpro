7d4af6f7f2c4944a5b80695b87d1a622bf7a9a2b
TrishabOnlineService <nitai.grp150@gmail.com>
2026-06-25 22:34:52 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
-const CACHE_NAME = 'duty-tracker-pro-v10';
-const STATIC_CACHE = 'dtp-static-v10';
-const DYNAMIC_CACHE = 'dtp-dynamic-v10';
+const CACHE_NAME = 'duty-tracker-pro-v11';
+const STATIC_CACHE = 'dtp-static-v11';
+const DYNAMIC_CACHE = 'dtp-dynamic-v11';
 
 const STATIC_ASSETS = [
   '/',
